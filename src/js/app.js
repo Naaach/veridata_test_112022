914e59e11371a7c9d03459547a0ad4138d2b43d7
Nacho <nach704@gmail.com>
2022-11-19 16:56:22 +0100

    listar los testimonios

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,30 @@
-window.addEventListener('load', windowLoadEvent => {
-	const store = {
-		testimonialsUrl: "https://testimonialapi.toolcarton.com/api",
-		testimonials: []
+function prevChecks() {
+	let validBrowser = true
+
+	// Comprobar si se puede hacer uso del elemento template
+	if (!('content' in document.createElement('template'))) {
+		alert("Se requiere un navegador que soporte <template>")
+		validBrowser = false
 	}
 
+	return validBrowser
+}
+
+window.addEventListener('load', windowLoadEvent => {
+	if (prevChecks() == false) return;
+
+	const store = {
+		testimonialsUrl: "https://testimonialapi.toolcarton.com/api",
+		testimonials: [],
+		elements: {
+			mainContainer: document.querySelector('section.main-container'),
+			testimonialsTemplate: document.querySelector('template#testimonial')
+		}
+	}
+
+	window.store = store
+
+	// Obtener los testimonios de la api
 	function getTestimonials() {
 		return new Promise((resolve, reject) => {
 			fetch(store.testimonialsUrl)
@@ -13,8 +34,44 @@ window.addEventListener('load', windowLoadEvent => {
 					store.testimonials = testimonials
 					resolve(testimonials)
 				})
+				.catch(err => {
+					console.log(err)
+					alert('Error obteniendo los testimonios')
+				}) 
 		})
 	}
 
-	getTestimonials().then(() => console.log(store.testimonials))
+	getTestimonials().then(() => {
+		console.log(store.testimonials)
+		store.testimonials.forEach(t => addTestimonialToBody(t))
+	})
+
+	// Pintamos los testimonios
+	function addTestimonialToBody(testimonial) {
+		const clone = store.elements.testimonialsTemplate.content.cloneNode(true);
+		const $testimonial = clone.querySelector('div.testimonial')
+
+		// Añadimos el avatar
+		const $avatar = $testimonial.querySelector('img.avatar')
+		if ($avatar) {
+			$avatar.setAttribute('src', testimonial.avatar || '')
+			$avatar.setAttribute('alt', testimonial.name || '')
+		}
+
+		// Añanimos los titulos
+		const $title = $testimonial.querySelector('.header .title-container .title')
+		if ($title) $title.innerText = testimonial.name || ''
+		const $subtitle = $testimonial.querySelector('.header .title-container .subtitle')
+		if ($subtitle) $subtitle.innerText = testimonial.location || ''
+
+		// Añadimos el texto
+		const $text = $testimonial.querySelector('.text')
+		if ($text) $text.innerText = testimonial.message || ''
+
+		// Añadir el rating
+		const $rating = $testimonial.querySelector('.rating .bold')
+		if ($rating) $rating.innerText = testimonial.rating || ''
+
+		store.elements.mainContainer.appendChild($testimonial)
+	}
 })
